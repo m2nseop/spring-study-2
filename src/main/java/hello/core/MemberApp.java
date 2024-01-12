952af96195e3c20d55e37b01506f4d1e71fceb44
m2nseop <pms246@naver.com>
2024-01-12 22:29:46 +0900

    feat: AppConfig를 통해 DI하여 OCP와 DIP를 준수

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,15 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
 
 public class MemberApp {
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        // appConfig에서 service 구현체를 받아오기만 하면 된다.
+        // 설사 구현체가 바뀌더라도 위 코드에 손댈 필요도 없다.
+        // 이게 의존관계 주입=DI이며 DI의 장점이다.
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         // Long 타입이라 L을 붙여줘야 함
         memberService.join(member);
