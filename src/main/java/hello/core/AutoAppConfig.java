7d759409344afe97e05edbf1a434f298ac9c80d6
m2nseop <pms246@naver.com>
2024-01-22 18:41:48 +0900

    test: 생성자 주입 옵션처리

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,9 +6,9 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member", // = {"" ,""} 이런식으로 여러개도 지정가능
-        // 탐색할 패키지의 시작 위치 지정 -> 모든 자바 클래스를 다 컴포넌트 스캔하면 시간이 오래걸리기 때문
-        basePackageClasses = AutoAppConfig.class, // 이런식으로 클래스를 지정하여, 해당 클래스가 존재하는 패키지를 탐색할 수도 있음
+//        basePackages = "hello.core.member", // = {"" ,""} 이런식으로 여러개도 지정가능
+//        // 탐색할 패키지의 시작 위치 지정 -> 모든 자바 클래스를 다 컴포넌트 스캔하면 시간이 오래걸리기 때문
+//        basePackageClasses = AutoAppConfig.class, // 이런식으로 클래스를 지정하여, 해당 클래스가 존재하는 패키지를 탐색할 수도 있음
 
         // 아무것도 지정을 안한다면? 디폴트 스캔 시작 위치는 어디인가?
         // @ComponentScan 이 붙은 설정 정보 클래스의 패키지가 시작 위치가 된다.
